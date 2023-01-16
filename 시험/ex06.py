7eb3f8c138f584bd31426ceae1985dd627866898
ceonnayoung <nayoung109191@gmail.com>
2023-01-16 13:10:07 +0900

    응용

---
+++ 시험/ex06.py
@@ -0,0 +1,8 @@
+#리스트는 파이썬이 제공하는 6개의 시퀸스 타입 중 하나힙니다.
+#시퀸스: 배열, 순서, 수순, 연속적인 사건들을 일컫는다.즉, 시퀸스 타입이란 데이터를 배열
+#및 순서대로 관리할 수 있는 타입을 의미한다
+egoing = ['programmer', 'seoul', 25, False]
+egoing[1] = 'busan'
+print(egoing[3])
+print(egoing)
+print(egoing[2])
